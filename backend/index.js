0b09f04f7db4a7d956622e28d0e7da71aa6bb603
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-14 02:52:33 +0530

    some

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,16 @@ import messageRouter from "./routers/message.router.js";
 import usersRouter from "./routers/users.router.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { app, server } from "./socket/socket.js";
 import path from "path";
 
+const __dirname = path.resolve();
+
+app.use(cors());
 app.use(express.json());
 dotenv.config();
 app.use(cookieParser());
-
-mongoose.connect(process.env.MONGODB).then(() => {
-  console.log("Connected to MongoDB");
-});
-const __dirname = path.resolve();
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
@@ -27,6 +26,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log("Connected to MongoDB");
+});
 server.listen(1000, () => {
   console.log("Server is running on port 1000");
 });
