008f26206b25552dde0d749491cc9268e5d2ddff
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-13 10:53:25 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+server.listen(1000, () => {
+  console.log("Server is running on port 1000");
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -37,8 +41,4 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
-});
-
-server.listen(1000, () => {
-  console.log("Server is running on port 1000");
 });
