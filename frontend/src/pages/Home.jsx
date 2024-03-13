2e58a6c30a6097e62d351194c5691e26d033b31b
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-14 05:18:24 +0530

    2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import SideBar from "../components/SideBar";
 
 export const Home = () => {
   return (
-    <div className="flex items-center justify-center h-[100dvh]">
+    <div className="flex items-center justify-center h-full">
       <div className="w-[100%] md:w-[80%] flex flex-col md:flex-row min-h-[10%] h-[100%] md:h-[80%]">
         <SideBar />
         <Message />
